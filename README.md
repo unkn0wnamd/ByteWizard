53e7a28844eabfc031c9f313ce56a418d2c9bb84
unkn0wnamd <114957830+unkn0wnamd@users.noreply.github.com>
2022-10-04 01:11:30 +0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
-# ByteWizard
+# Byte Wizard 🧙
+![image](https://user-images.githubusercontent.com/58490288/193213111-041e3304-33e7-4a3e-8997-4c9660d4a52b.png)
+
+Our goal is to make a tool that changes the files signatures AKA (Magic Bytes) to help pentesters in bypassing file upload security functions.
+
+## Authors:
+- Ahmad Bahkali [@unknownamd_](https://twitter.com/unknownamd_)
+- Mohammed Aljayzani [@z5jt4](https://twitter.com/z5jt4)
+- Yousef Alfuhaid [@Pho1yalfuhaid](https://twitter.com/Pho1yalfuhaid)
+
+## installing the tool:
+```
+git clone https://github.com/z5jt/ByteWizard.git
+cd ByteWizard
+pip3 install -r requirements.txt
+python3 ByteWizard.py
+```
+## v1.0:
+
+### Note (Next Step):
+- Metadata changer 
+
+
+
+### v1.2:
+Soon..
+
+- PHP shell code injection in files. 
+
+للمجد قادمون
+
+#### Disclaimer
+Usage of ~sqlmap~ **Byte Wizard** for attacking targets without prior mutual consent is illegal. It is the end user's responsibility to obey all applicable local, state and federal laws. Developers assume no liability and are not responsible for any misuse or damage caused by this program.
---
+++ aaaa.py
@@ -0,0 +1,126 @@
+#!/usr/bin/env python3
+from art import *
+import time
+import sys
+from termcolor import colored
+
+
+def banner():
+    print(""" 
+                             .
+                           .
+                    /^\     .
+               /\   "V"
+              /__\   I      O  o
+             //..\\\  I     .
+             \].`[/  I
+             /l\/j\  (]    .  O
+            /. ~~ ,\/I          .
+            \\L__j^\//I       o
+             \/--v}  I     o   .
+             |    |  I   _________
+             |    |  I c(`       ')o
+             |    l  I   \.     ,/
+           _/j  L l\_!  _//^---^\\_  
+                                                                                              
+ ------------------- Twitter -------------------
+  ----- @z5jt4 @unknownamd_ @Pho1yalfuhaid -----""")
+
+    text = text2art("Byte Wizard")
+    print(text)
+
+
+def loading():
+    print("Changing file signature: ")
+    animation = ["[■□□□□□□□□□]", "[■■□□□□□□□□]", "[■■■□□□□□□□]", "[■■■■□□□□□□]", "[■■■■■□□□□□]", "[■■■■■■□□□□]",
+                 "[■■■■■■■□□□]", "[■■■■■■■■□□]", "[■■■■■■■■■□]", "[■■■■■■■■■■]"]
+    for i in range(len(animation)):
+        time.sleep(0.2)
+        sys.stdout.write("\r" + animation[i % len(animation)])
+        sys.stdout.flush()
+
+
+def line_prepender(filename, line):
+    with open(filename, 'r+') as f:
+        content = f.read()
+        f.seek(0, 0)
+        # f.write(line + content)
+        f.write(line.rstrip('\r\n') + '\n' + content)
+
+
+def file_header(filename, filetype):
+    with open(filename, 'rb+') as f:
+        f.seek(0, 0)
+        f.write(filetype)
+        f.close()
+
+
+def file_changer():
+    print("""Select the filetype that you want to convert to:
+    1- PDF   2- MP3 (audio)   3- jpg    4- pcapng
+    5- GIF   6- tar           7- Word-Office
+    
+              
+    Insert q to Quit 
+-------------------------------------------------""")
+
+    try:
+        user_input = input("choose a filetype:")
+    except ValueError:
+        err = colored("ERROR: Only Numbers are Accepted", 'red')
+        print(err)
+        time.sleep(1)
+        file_changer()
+
+    options = ["1", "2", "3", "4", "5","6","7","q"]
+    while user_input in options:
+        if user_input == "1":
+            file_type = b'\x25\x50\x44\x46\x2D'
+            file_newline = "AAAAA\n"
+        elif user_input == "2":
+            file_type = b'\x49\x44\x33'
+            file_newline = "AAA\n"
+        elif user_input == "3":
+            file_type = b'\xFF\xD8\xFF\xE0'
+            file_newline = "AAAA\n"
+        elif user_input == "4":
+            file_type = b'\xD4\xC3\xB2\xA1'
+            file_newline = "AAAA\n"
+        elif user_input == "5":
+            file_type = b'\x47\x49\x46\x38\x39\x61'
+            file_newline = "AAAAAA\n"
+        elif user_input == "6":
+            file_type = b'\x1F\x9D'
+            file_newline = "AA\n"
+        elif user_input == "7":
+            file_type = b'\xD0\xCF\x11\xE0\xA1\xB1\x1A\xE1'
+            file_newline = "AAAAAAAA\n"
+        elif user_input == "q":
+            print("\nQUITTING!!")
+            sys.exit()
+        
+        try:
+            file = input("Enter a file name : ")
+            line_prepender(file, file_newline)
+            file_header(file, file_type)
+            loading()
+            success = colored(" \n✔ Your file {} type have been changed successfully by Byte Wizard.", 'green')
+            print(success.format(file))
+            exit()
+        except FileNotFoundError:
+            err = colored("ERROR 404: File {} not found", 'red')
+            print(err.format(file))
+    else:
+        err = colored("\nERROR: Choose a filetype from the list :)", 'red')
+        print(err)
+        return file_changer()
+
+
+# ADD METADATA FUNCTION HERE
+
+banner()
+try:
+    file_changer()
+except KeyboardInterrupt:
+    print("\nQUITTING!!")
+    sys.exit()
